bf6bca314e1f05cf45b76d67d9e24a48032231ff
Oliver Nygren <oliver.nygren@icloud.com>
2020-10-23 14:46:17 +0200

    added JSDoc comments

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 
+/**
+ * Will generate a rondom number and display it on the DOM
+ */
 function printRandomNumber() {
     const randomNumber = getRandomNumber();
     addNumberToPage(randomNumber);
 }
 
+/**
+ * Generates a random number between 0-100 and returns it
+ * @returns {number} the random number that was generated
+ */
 function getRandomNumber() {
     const randomNumber = Math.random() * 100;
     return Math.round(randomNumber);
 }
 
+/**
+ * Places the passed number into the DOM along with a comma and space
+ * @param {Number} number number that will be displayen in the DOM
+ */
 function addNumberToPage(number) {
     const div = document.getElementById('results');
     div.append(number + ', ');
