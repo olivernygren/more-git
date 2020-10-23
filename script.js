21d9c6c76ad5d4ca119126db699f48b2a3ea0217
Oliver Nygren <oliver.nygren@icloud.com>
2020-10-23 13:53:38 +0200

    randomized number functions

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+
 function printRandomNumber() {
-    console.log('hej');
+    const randomNumber = getRandomNumber();
+    addNumberToPage(randomNumber);
+}
+
+function getRandomNumber() {
+    const randomNumber = Math.random() * 100;
+    return Math.round(randomNumber);
+}
+
+function addNumberToPage(number) {
+    console.log(number);
 }
